7a81e022470da4f40eda04317e3310c5aebf7d3c
Sid <siddharthpradeep4602@gmail.com>
2022-01-15 08:24:58 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, TextInput } from 'react-native';    //CSS stuff is imported here
+import { ImageBackground } from 'react-native-web';
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color: 'blue'}}>Hi my  is Siddharth P, 20BLC1071 and this is my first app</Text>
+      <Image
+      source = {{
+          uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+        }}
+        style={{width: 200, height: 200}}
+      />
       <StatusBar style="auto" />
+      <View style={styles.container2}>
+        <TextInput
+          placeholder = "Input text here"
+          keyboardType='numeric'
+        />
+      </View>
     </View>
+  
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text1: {
+    color: 'blue'
+  },
+  container2: {
+    color: 'black',
+    backgroundColor: 'white',
+    width: 100
   },
 });
